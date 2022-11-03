e71f8fcb72b347c7ba1d3137e7c3a6af3fc271f2
1AsTaP1 <sasha.ostapenko99@gmail.com>
2022-11-03 19:24:31 +0300

    start_files

---
+++ Kurs/exam 1.py
@@ -0,0 +1,74 @@
+# Задача 1
+# print(4 ** 4 ** 4)
+
+# Задача 2
+# import math
+# a = 1
+# b = 1 / ((1 + a + a**2)/(2 * a + a ** 2) + 2 - (1 - a + a ** 2)/(2 * a - a ** 2))
+# c = 5 - 2 * a ** 2
+# y = b * c
+# print(y)
+# a = 90
+# b = 90
+# y = 90
+# p = 0.25 * (math.sin(a+b-y) + math.sin(b + y - a) + math.sin(y + a - b) - math.sin(a + b + y))
+# print(p)
+
+# Задача 3
+# a = []
+# print(bool(a)) # Значит, что принимает на себя значение 0, если написать в 18 строк любое значение int, то будет True
+
+# Задача 4
+# m = int(input())
+# n = int(input())
+# for i in range(m, n + 1):
+#     if m <= n:
+#         print(i)
+
+# Задача 5
+# m = int(input())
+# n = int(input())
+# if m < n:
+#     for i in range(m, n + 1):
+#         print(i)
+# else:
+#     for i in reversed(range(n, m + 1)):
+#         print(i)
+
+# Задача 6
+# a = 'Иван Иванов'
+# print(a[5:] + ' ' + a[:4])
+
+# Задача 7
+# a = []
+# for i in range(1, 51):
+#     a.append(i)
+# print(a)
+
+# Задача 8
+# a = [1, 5, 2, 9, 2, 9, 1]
+# for n in a:
+#     if a.count(n) == 1:
+#       print(a)
+
+# Задача 9
+# a = ["student1", "student2", "student3"]
+# b = []
+# for i in a:
+#     b.append(i + '_good')
+# a = b
+# print(b)
+
+# Задача 10
+# for i in range(0, 51):
+#     if i == 35:
+#         continue
+#     print(i)
+
+# Задача 11
+# a = ["hello my friend", "my name is", 'house', 'cat', 'dog']
+# b = []
+# for i in a:
+#     if i.find(' ') != -1:
+#         b.append(i)
+# print(b)
